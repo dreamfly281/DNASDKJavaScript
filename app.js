47bedfb91071c441a3ed40604491fde87d71e858
liyingxuan <mvp_xuan@163.com>
2017-11-20 16:44:19 +0800

    Optimized display.

--- app.js
+++ app.js
@@ -1783,10 +1783,10 @@ var transactionRecordDateTime = function(inputTime)  {
   var date = new Date(inputTime);
   Y = date.getFullYear() + '-';
   M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  D = date.getDate();
-  h = date.getHours();
-  m = date.getMinutes();
-  s = date.getSeconds();
+  D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  h = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+  m = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+  s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
   return Y + M + D + ' ' + h + ':' + m + ':' + s;
 };
 
